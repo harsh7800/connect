fd1d37b227310f4c60e6149fc9cc0447e1a3d23a
Harry <harshshinde5606@gmail.com>
2024-04-07 19:47:29 +0530

    gap between checkbox and label on meetingSetup

--- components/meetingSetup.tsx
+++ components/meetingSetup.tsx
@@ -33,18 +33,18 @@ const MeetingSetup = ({
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
       <div className="flex h-16 items-center justify-center gap-3">
+        <Checkbox
+          className="border-1 bg-white rounded-md px-3 py-3 "
+          id="Toggle-Mic/Camera"
+          checked={isMicCamTogledOn}
+          onCheckedChange={(e: boolean) => {
+            setIsMicCamTogledOn(e);
+          }}
+        />
         <label
           className="flex items-center select-none cursor-pointer justify-center gap-0 font-medium"
           htmlFor="Toggle-Mic/Camera"
         >
-          <Checkbox
-            className="border-1 bg-white rounded-md px-3 py-3 "
-            id="Toggle-Mic/Camera"
-            checked={isMicCamTogledOn}
-            onCheckedChange={(e: boolean) => {
-              setIsMicCamTogledOn(e);
-            }}
-          />
           Join with Muted and camera off
         </label>
         <DeviceSettings />
